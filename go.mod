ad8816cfcbcd249fbb5fc76fe75e9f4fa5f1be0c
Javohir Khasanov <javohdev@gmail.com>
2025-02-03 18:26:09 +0500

    Mini twitter running on Kubernetsgit add .git add .git add .git add .!

--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.15.1
 	github.com/golanguzb70/redis-cache v1.1.0
-	github.com/google/generative-ai-go v0.19.0
 	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.2.6
 	github.com/jackc/pgconn v1.10.1
@@ -24,16 +23,9 @@ require (
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.4
 	golang.org/x/crypto v0.31.0
-	google.golang.org/api v0.186.0
 )
 
 require (
-	cloud.google.com/go v0.115.0 // indirect
-	cloud.google.com/go/ai v0.8.0 // indirect
-	cloud.google.com/go/auth v0.6.0 // indirect
-	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
-	cloud.google.com/go/compute/metadata v0.3.0 // indirect
-	cloud.google.com/go/longrunning v0.5.7 // indirect
 	github.com/BurntSushi/toml v0.4.1 // indirect
 	github.com/Eun/go-convert v1.2.12 // indirect
 	github.com/Eun/go-doppelgangerreader v0.0.0-20190911075941-30f1527f16b2 // indirect
@@ -45,10 +37,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
@@ -57,12 +46,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.12.5 // indirect
 	github.com/gookit/color v1.4.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
@@ -100,23 +85,13 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.51.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0 // indirect
-	go.opentelemetry.io/otel v1.26.0 // indirect
-	go.opentelemetry.io/otel/metric v1.26.0 // indirect
-	go.opentelemetry.io/otel/trace v1.26.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/oauth2 v0.21.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/tools v0.28.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240617180043-68d350f18fd4 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240617180043-68d350f18fd4 // indirect
 	google.golang.org/grpc v1.64.1 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -24,7 +24,6 @@ import (
 // @title       Go Clean Template API
 // @description This is a sample server Go Clean Template server.
 // @version     1.0
-// @host        localhost:8080
 // @BasePath    /v1
 // @securityDefinitions.apikey BearerAuth
 // @in header
@@ -36,13 +35,13 @@ func NewRouter(engine *gin.Engine, l *logger.Logger, config *config.Config, useC
 
 	handlerV1 := handler.NewHandler(l, config, useCase, redis)
 
+	// Swagger - Place this before AuthMiddleware
+	url := ginSwagger.URL("swagger/doc.json") // The URL pointing to API definition
+	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
 	// Initialize Casbin enforcer
 	e := casbin.NewEnforcer("config/rbac.conf", "config/policy.csv")
-	engine.Use(handlerV1.AuthMiddleware(e))
-
-	// Swagger
-	url := ginSwagger.URL("swagger/doc.json") // The url pointing to API definition
-	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+	engine.Use(handlerV1.AuthMiddleware(e)) // Apply authentication middleware to all routes except Swagger
 
 	// K8s probe
 	engine.GET("/healthz", func(c *gin.Context) { c.Status(http.StatusOK) })
@@ -51,55 +50,88 @@ func NewRouter(engine *gin.Engine, l *logger.Logger, config *config.Config, useC
 	engine.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	// Routes
-	v1 := engine.Group("/v1")
-
-	user := v1.Group("/user")
+	v1 := engine.Group("/v1") 
 	{
-		user.POST("/", handlerV1.CreateUser)
-		user.GET("/list", handlerV1.GetUsers)
-		user.GET("/:id", handlerV1.GetUser)
-		user.PUT("/", handlerV1.UpdateUser)
-		user.DELETE("/:id", handlerV1.DeleteUser)
+		v1.POST("/user", handlerV1.CreateUser)
+		v1.GET("/user/list", handlerV1.GetUsers)
+		v1.GET("/user/:id", handlerV1.GetUser)
+		v1.PUT("/user", handlerV1.UpdateUser)
+		v1.DELETE("/user/:id", handlerV1.DeleteUser)
+
+		v1.GET("/session/list", handlerV1.GetSessions)
+		v1.GET("/session/:id", handlerV1.GetSession)
+		v1.PUT("/session", handlerV1.UpdateSession)
+		v1.DELETE("/session/:id", handlerV1.DeleteSession)
+
+		v1.POST("/auth/logout", handlerV1.Logout)
+		v1.POST("/auth/register", handlerV1.Register)
+		v1.POST("/auth/verify-email", handlerV1.VerifyEmail)
+		v1.POST("/auth/login", handlerV1.Login)
+
+		v1.POST("/tag", handlerV1.CreateTag)
+		v1.GET("/tag/list", handlerV1.GetTags)
+		v1.GET("/tag/:id", handlerV1.GetTag)
+		v1.PUT("/tag", handlerV1.UpdateTag)
+		v1.DELETE("/tag/:id", handlerV1.DeleteTag)
+
+		v1.POST("/follower", handlerV1.FollowUnfollow)
+		v1.GET("/follower/list", handlerV1.GetFollowers)
+
+		v1.POST("/tweet", handlerV1.CreateTweet)
+		v1.GET("/tweet/list", handlerV1.GetTweets)
+		v1.GET("/tweet/:id", handlerV1.GetTweet)
+		v1.PUT("/tweet", handlerV1.UpdateTweet)
+		v1.DELETE("/tweet/:id", handlerV1.DeleteTweet)
+
+		
 	}
 
-	session := v1.Group("/session")
-	{
-		session.GET("/list", handlerV1.GetSessions)
-		session.GET("/:id", handlerV1.GetSession)
-		session.PUT("/", handlerV1.UpdateSession)
-		session.DELETE("/:id", handlerV1.DeleteSession)
-	}
+	// user := v1.Group("/user")
+	// {
+	// 	user.POST("/user", handlerV1.CreateUser)
+	// 	user.GET("/user/list", handlerV1.GetUsers)
+	// 	user.GET("/user/:id", handlerV1.GetUser)
+	// 	user.PUT("/user/", handlerV1.UpdateUser)
+	// 	user.DELETE("/user/:id", handlerV1.DeleteUser)
+	// }
 
-	auth := v1.Group("/auth")
-	{
-		auth.POST("/logout", handlerV1.Logout)
-		auth.POST("/register", handlerV1.Register)
-		auth.POST("/verify-email", handlerV1.VerifyEmail)
-		auth.POST("/login", handlerV1.Login)
-	}
+	// session := v1.Group("/session")
+	// {
+	// 	session.GET("/session/list", handlerV1.GetSessions)
+	// 	session.GET("/session/:id", handlerV1.GetSession)
+	// 	session.PUT("/session", handlerV1.UpdateSession)
+	// 	session.DELETE("/session/:id", handlerV1.DeleteSession)
+	// }
 
-	tag := v1.Group("/tag")
-	{
-		tag.POST("/", handlerV1.CreateTag)
-		tag.GET("/list", handlerV1.GetTags)
-		tag.GET("/:id", handlerV1.GetTag)
-		tag.PUT("/", handlerV1.UpdateTag)
-		tag.DELETE("/:id", handlerV1.DeleteTag)
-	}
+	// auth := v1.Group("/auth")
+	// {
+	// 	auth.POST("/auth/logout", handlerV1.Logout)
+	// 	auth.POST("/auth/register", handlerV1.Register)
+	// 	auth.POST("/auth/verify-email", handlerV1.VerifyEmail)
+	// 	auth.POST("/auth/login", handlerV1.Login)
+	// }
 
-	follower := v1.Group("/follower")
-	{
-		follower.POST("/", handlerV1.FollowUnfollow)
-		follower.GET("/list", handlerV1.GetFollowers)
-	}
+	// tag := v1.Group("/tag")
+	// {
+	// 	tag.POST("/tag", handlerV1.CreateTag)
+	// 	tag.GET("/tag/list", handlerV1.GetTags)
+	// 	tag.GET("/tag/:id", handlerV1.GetTag)
+	// 	tag.PUT("/tag", handlerV1.UpdateTag)
+	// 	tag.DELETE("/tag/:id", handlerV1.DeleteTag)
+	// }
 
-	tweet := v1.Group("/tweet")
-	{
-		tweet.POST("/", handlerV1.CreateTweet)
-		tweet.GET("/list", handlerV1.GetTweets)
-		tweet.GET("/:id", handlerV1.GetTweet)
-		tweet.PUT("/", handlerV1.UpdateTweet)
-		tweet.DELETE("/:id", handlerV1.DeleteTweet)
-	}
+	// follower := v1.Group("/follower")
+	// {
+	// 	follower.POST("/follower", handlerV1.FollowUnfollow)
+	// 	follower.GET("/follower/list", handlerV1.GetFollowers)
+	// }
 
+	// tweet := v1.Group("/tweet")
+	// {
+	// 	tweet.POST("/tweet", handlerV1.CreateTweet)
+	// 	tweet.GET("/tweet/list", handlerV1.GetTweets)
+	// 	tweet.GET("/tweet/:id", handlerV1.GetTweet)
+	// 	tweet.PUT("/tweet", handlerV1.UpdateTweet)
+	// 	tweet.DELETE("/tweet/:id", handlerV1.DeleteTweet)
+	// }
 }
